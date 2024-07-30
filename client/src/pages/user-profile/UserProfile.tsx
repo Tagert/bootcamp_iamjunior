e64d0c6797abd9e1cec2a29c43423a0f62f05264
Tagert <mini.andrius@gmail.com>
2024-07-30 09:59:15 +0300

    :sparkles: feat(Profile): Added tabs for profile page.

--- client/src/pages/user-profile/UserProfile.tsx
+++ client/src/pages/user-profile/UserProfile.tsx
@@ -1,5 +1,7 @@
 import styles from "./UserProfile.module.scss";
 import { Page } from "../../components/template/Page";
+import { Tabs, rem } from "@mantine/core";
+import { IconUserScan, IconMapPin, IconLock } from "@tabler/icons-react";
 import { UserContactForm } from "../../components/user-profile/UserContactForm/UserContactForm";
 import { UserLocationForm } from "../../components/user-profile/UserLocationForm/UserLocationForm";
 import { UserPasswordChangeForm } from "../../components/user-profile/UserPasswordChangeForm/UserPasswordChangeForm";
@@ -7,6 +9,8 @@ import { useAuthStore } from "../../store/use-auth.store";
 
 export const UserProfile = () => {
   const { user } = useAuthStore();
+
+  const iconStyle = { width: rem(24), height: rem(24) };
 
   return (
     <Page>
@@ -16,16 +20,52 @@ export const UserProfile = () => {
         </div>
 
         <section className={styles.formsContainer}>
-          <UserContactForm className={styles.formHolder} />
+          <Tabs
+            variant="outline"
+            radius="md"
+            defaultValue="contact"
+            classNames={{ list: styles.list, tabLabel: styles.tabLabel }}
+          >
+            <Tabs.List>
+              <Tabs.Tab
+                value="contact"
+                leftSection={<IconUserScan style={iconStyle} />}
+              >
+                Contact
+              </Tabs.Tab>
 
-          <UserLocationForm className={styles.formHolder} />
+              <Tabs.Tab
+                value="location"
+                leftSection={<IconMapPin style={iconStyle} />}
+              >
+                Location
+              </Tabs.Tab>
 
-          {user && (
-            <UserPasswordChangeForm
-              className={styles.formHolder}
-              user_id={user.id}
-            />
-          )}
+              <Tabs.Tab
+                value="password"
+                leftSection={<IconLock style={iconStyle} />}
+              >
+                Password
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="contact">
+              <UserContactForm className={styles.formHolder} />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="location">
+              <UserLocationForm className={styles.formHolder} />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="password">
+              {user && (
+                <UserPasswordChangeForm
+                  className={styles.formHolder}
+                  user_id={user.id}
+                />
+              )}
+            </Tabs.Panel>
+          </Tabs>
         </section>
       </main>
     </Page>
