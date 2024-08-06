049e925dc4024ecafc819812e36d6a9ac9cd38bb
Tagert <mini.andrius@gmail.com>
2024-08-06 09:08:37 +0300

    :recycle: refactor(UserModel): Updated UserModel schema.

--- server/src/config/swagger/schemas/user-sw.schema.ts
+++ server/src/config/swagger/schemas/user-sw.schema.ts
@@ -10,17 +10,47 @@ export const UserSchema = {
       type: "string",
       description: "User name",
     },
+    age: {
+      type: "number",
+      description: "User age",
+    },
     email: {
       type: "string",
-      description: "User email",
+      description: "User email and login info",
     },
     password: {
       type: "string",
       description: "User password",
     },
+    gender: {
+      type: "string",
+      description: "User gender",
+    },
+    phone_number: {
+      type: "string",
+      description: "User phone number",
+    },
+    contact_email: {
+      type: "string",
+      description: "Alternative contact email",
+    },
+    city: {
+      type: "string",
+      description: "User city",
+    },
+    address: {
+      type: "string",
+      description: "User address",
+    },
+    birthday: {
+      type: "string",
+      format: "date",
+      description: "User birthday",
+    },
     role: {
       type: "string",
       description: "User role",
+      default: "user",
     },
     favorites: {
       type: "array",
@@ -28,6 +58,16 @@ export const UserSchema = {
         type: "string",
       },
       description: "List of user's favorite business IDs",
+    },
+    created_at: {
+      type: "string",
+      format: "date-time",
+      description: "Timestamp when the user was created",
+    },
+    updated_at: {
+      type: "string",
+      format: "date-time",
+      description: "Timestamp when the user was last updated",
     },
   },
 };
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -5,6 +5,12 @@ type UserBody = {
   age: number;
   email: string;
   password: string;
+  gender: string;
+  phone_number: string;
+  contact_email: string;
+  city: string;
+  address: string;
+  birthday: Date;
   role: string;
   favorites: [string];
 };
@@ -19,6 +25,12 @@ const userSchema = new mongoose.Schema<UserType>(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    gender: { type: String, required: false },
+    phone_number: { type: String, required: false },
+    contact_email: { type: String, required: false },
+    city: { type: String, required: false },
+    address: { type: String, required: false },
+    birthday: { type: Date, required: false },
     role: { type: String, required: true, default: "user" },
     favorites: { type: [String], required: false, default: [] },
   },
